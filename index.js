a2bc436c6848ccde1ab0830487b9938725c71f17
david-chua <dp_chua@hotmail.com>
2019-03-19 22:01:00 -0400

    added get routes for cohort

--- index.js
+++ index.js
@@ -11,6 +11,30 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 
+server.get('/api/cohorts', async(req,res) => {
+  try{
+    const cohorts = await db('cohorts');
+    res.status(200).json(cohorts);
+  } catch (error){
+    res.status(500).json(error);
+  }
+});
+
+server.get('/api/cohorts/:id', async(req,res) => {
+  try{
+    const cohort = await db('cohorts')
+      .where({ id: req.params.id })
+      .first();
+    res.status(200).json(cohort);
+  } catch(error){
+    res.status(500).json(error);
+  }
+});
+
+const errors = {
+  '19': 'Another record with that value exists',
+};
+
 
 
 const port = process.env.PORT || 9090;
